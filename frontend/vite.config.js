498ebc827d61f3fe2a1ca54d966d7aa4c915bbd1
Ivan Gyimah <ivancomovis@gmail.com>
2025-05-28 18:29:47 +0000

    saving changes, chnaged the build method

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 1601,
+        strictPort: true,
+        host: '0.0.0.0',
+        open: true,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@lib': path.resolve(__dirname, './src/lib') // Add this line
+        },
+    },
+});
